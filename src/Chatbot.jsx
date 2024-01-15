ea580aa7e3f683ec4862feabaab1b2ce82d56347
Sifat Sikder <bsse1221@iit.du.ac.bd>
2024-01-15 15:01:41 +0600

    created user form

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AWS from "aws-sdk";
+import "./Chatbot.css";
 
 const Chatbot = () => {
   const [userInput, setUserInput] = useState("");
@@ -31,14 +32,24 @@ const Chatbot = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
-        placeholder="Type your message..."
-      />
-      <button onClick={sendMessage}>Send</button>
+    <div className="center-square ">
+      <div className="square">
+        <div className="w-full flex flex-col mt-96 p-2">
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder="Type your message..."
+            className="border border-2 border-solid border-black w-full mt-24 rounded-md"
+          />
+          <button
+            onClick={sendMessage}
+            className="bg-blue-500 text-white w-1/3 mt-2 p-2 rounded-md ml-64"
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
